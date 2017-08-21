cca9717c4d4fbd8fa08f960296f102345ec5631d
Humberto Maquilon <hmaquilon@t-sciences.com>
2017-08-21 11:10:40 -0400

    Added notes routes

---
+++ server.js
@@ -0,0 +1,20 @@
+// Skeleton for a back-end for a note-taking application
+// CRUD actions
+// Setup a database in Mongo. For this example: mongodb://localhost:27017/notes
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+})
